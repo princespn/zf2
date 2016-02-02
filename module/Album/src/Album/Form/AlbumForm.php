86d2c257fcab70cd0128d0682381b887b14263ea
amit tiwari <amittiwarihdrc@gmail.com>
2016-02-02 16:31:45 +0530

    second push

---
+++ module/Album/src/Album/Form/AlbumForm.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Album\Form;
+
+ use Zend\Form\Form;
+
+ class AlbumForm extends Form
+ {
+     public function __construct($name = null)
+     {
+         // we want to ignore the name passed
+         parent::__construct('album');
+
+         $this->add(array(
+             'name' => 'id',
+             'type' => 'Hidden',
+         ));
+         $this->add(array(
+             'name' => 'title',
+             'type' => 'Text',
+             'options' => array(
+                 'label' => 'Title',
+             ),
+         ));
+         $this->add(array(
+             'name' => 'artist',
+             'type' => 'Text',
+             'options' => array(
+                 'label' => 'Artist',
+             ),
+         ));
+         $this->add(array(
+             'name' => 'submit',
+             'type' => 'Submit',
+             'attributes' => array(
+                 'value' => 'Go',
+                 'id' => 'submitbutton',
+             ),
+         ));
+     }
+ }
